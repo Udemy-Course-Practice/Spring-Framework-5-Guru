db33ae047fa6caa25eec56011966f7cd73ce6302
VinhLB <dongoc409@gmail.com>
2023-01-27 09:50:07 +0700

    init

---
+++ Dependency-injection/src/main/java/spring5/dependencyinjection/DependencyInjectionApplication.java
@@ -0,0 +1,15 @@
+package spring5.dependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+@Profile(na)
+public class DependencyInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjectionApplication.class, args);
+    }
+
+}
